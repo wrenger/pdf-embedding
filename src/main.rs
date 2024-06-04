fdd007100ab9db88dcb1c83e76c0aa16f73835a1
Lars Wrenger <lars@wrenger.net>
2024-06-05 00:43:30 +0200

    :art: Add missing refs

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,89 @@
-use std::env::args;
+use std::{collections::BTreeMap, env::args};
 
-use pdf_writer::{Content, Name, Obj, PdfWriter, Rect, Ref, Str};
+use pdf_writer::{Content, Dict, Name, Obj, Pdf, Rect, Ref, Str};
 
 fn main() {
+    let mut ref_counter = Ref::new(1);
     let file = args().nth(1).expect("Usage: [file.pdf]");
 
-    let (image_rect, image_content, image_fonts) = extract_image(&file).unwrap();
+    let (image_rect, image_content, ressources, objects) = extract_image(&file).unwrap();
 
-    let catalog_id = Ref::new(1);
-    let page_tree_id = Ref::new(2);
-    let page_id = Ref::new(3);
-    let contents_id = Ref::new(4);
-    let image_id = Ref::new(5);
-    let font_id = Ref::new(6);
+    let mut pdf = Pdf::new();
+    let page_tree_id = ref_counter.bump();
+    pdf.catalog(ref_counter.bump()).pages(page_tree_id);
+
+    let page_id = ref_counter.bump();
+    let image_id = ref_counter.bump();
+    let font_id = ref_counter.bump();
 
     let font_name = Name(b"F1");
     let image_name = Name(b"myimg");
 
-    let mut writer = PdfWriter::new();
-    writer.catalog(catalog_id).pages(page_tree_id);
-    writer.pages(page_tree_id).kids([page_id]).count(1);
+    pdf.pages(page_tree_id).kids([page_id]).count(1);
 
     let page_rect = Rect::new(0.0, 0.0, 612.0, 792.0);
 
-    {
+    let mut indirects = {
         // create image object
-        let mut xobj = writer.form_xobject(image_id, &image_content);
+        let mut xobj = pdf.form_xobject(image_id, &image_content.content);
 
         xobj.bbox(image_rect);
 
-        let mut resources = xobj.resources();
-        let mut fonts_dict = resources.fonts();
-
-        // reference fonts
-        for (i, (name, _font)) in image_fonts.iter().enumerate() {
-            let id = Ref::new(i as i32 + 7);
-            fonts_dict.pair(Name(&name), id);
+        let mut indirects = Vec::new();
+        for (key, val) in image_content.dict.iter() {
+            convert_obj(
+                &mut ref_counter,
+                xobj.insert(Name(key)),
+                val,
+                &objects,
+                &mut indirects,
+            )
         }
+
+        // add ressources
+        let res_obj = xobj.insert(Name(b"Resources")).start();
+        convert_dict(
+            &mut ref_counter,
+            res_obj,
+            &ressources,
+            &objects,
+            &mut indirects,
+        );
+        indirects
+    };
+
+    // copy indirectly referenced objects
+    let mut remainder = Vec::new();
+    while !indirects.is_empty() {
+        while let Some((id, obj)) = indirects.pop() {
+            if let lopdf::Object::Stream(s) = obj {
+                let mut stream = pdf.stream(id, &s.content);
+                for (key, val) in s.dict.iter() {
+                    convert_obj(
+                        &mut ref_counter,
+                        stream.insert(Name(key)),
+                        val,
+                        &objects,
+                        &mut remainder,
+                    )
+                }
+            } else {
+                convert_obj(
+                    &mut ref_counter,
+                    pdf.indirect(id),
+                    obj,
+                    &objects,
+                    &mut remainder,
+                );
+            }
+        }
+        std::mem::swap(&mut remainder, &mut indirects);
     }
 
+    let contents_id = ref_counter.bump();
     {
         // create a new page
-        let mut page = writer.page(page_id);
+        let mut page = pdf.page(page_id);
         page.parent(page_tree_id)
             .contents(contents_id)
             .media_box(page_rect);
@@ -51,16 +93,7 @@ fn main() {
         resources.fonts().pair(font_name, font_id);
     }
 
-    // embed fonts
-    for (i, (_name, font)) in image_fonts.iter().enumerate() {
-        let id = Ref::new(i as i32 + 7);
-        let mut descriptor = writer.font_descriptor(id);
-        for (key, value) in font.into_iter() {
-            // just copy all elements
-            convert_obj(value, descriptor.insert(Name(&key)));
-        }
-    }
-    writer.type1_font(font_id).base_font(Name(b"Helvetica"));
+    pdf.type1_font(font_id).base_font(Name(b"Helvetica"));
 
     let desired_width = 500.0;
     let scale = desired_width / image_rect.x2;
@@ -85,18 +118,26 @@ fn main() {
         .transform([scale, 0.0, 0.0, scale, 0.0, 0.0]) // scale
         .x_object(image_name)
         .restore_state();
-    writer.stream(contents_id, &page_content.finish());
+    pdf.stream(contents_id, &page_content.finish());
 
-    std::fs::write("out.pdf", writer.finish()).unwrap();
+    std::fs::write("out.pdf", pdf.finish()).unwrap();
 }
 
-fn extract_image(file: &str) -> lopdf::Result<(Rect, Vec<u8>, Vec<(Vec<u8>, lopdf::Dictionary)>)> {
+fn extract_image(
+    file: &str,
+) -> lopdf::Result<(
+    Rect,
+    lopdf::Stream,
+    lopdf::Dictionary,
+    BTreeMap<lopdf::ObjectId, lopdf::Object>,
+)> {
     let doc = lopdf::Document::load(file)?;
-    println!("{doc:#?}");
     let page_id = doc.get_pages()[&1];
-    println!("{page_id:?}");
+    println!("\n\nPage: {page_id:?}");
+
     let meta = doc.get_dictionary(page_id)?;
-    println!("{meta:#?}");
+    println!("\n\nMeta: {meta:#?}");
+
     let media_box = meta
         .get(b"MediaBox")?
         .as_array()?
@@ -105,23 +146,45 @@ fn extract_image(file: &str) -> lopdf::Result<(Rect, Vec<u8>, Vec<(Vec<u8>, lopd
         .collect::<Vec<_>>();
     let rect = Rect::new(media_box[0], media_box[1], media_box[2], media_box[3]);
     println!("{:?}", meta.get(b"MediaBox")?);
-    let fonts = doc.get_page_fonts(page_id);
-    println!("{fonts:#?}");
 
-    let objects = doc.get_page_contents(page_id);
-    println!("{objects:?}");
+    // Find ressources
+    let ressources = match doc.get_page_resources(page_id) {
+        (Some(ressources), _) => ressources.clone(),
+        (None, ids) if ids.len() > 0 => doc.get_dictionary(ids[0])?.clone(),
+        _ => Default::default(),
+    };
 
-    let content = doc.get_page_content(page_id)?;
+    println!("\n\nRessources: {ressources:#?}");
 
-    Ok((
-        rect,
-        content,
-        fonts.into_iter().map(|(k, v)| (k, v.clone())).collect(),
-    ))
+    let contents = doc.get_page_contents(page_id);
+    assert_eq!(contents.len(), 1);
+    let lopdf::Object::Stream(stream) = doc.get_object(contents[0])? else {
+        return Err(lopdf::Error::PageNumberNotFound(1));
+    };
+
+    Ok((rect, stream.clone(), ressources, doc.objects))
+}
+
+fn convert_dict<'a, 'b: 'a>(
+    ref_counter: &mut Ref,
+    mut into: Dict<'a>,
+    from: &'b lopdf::Dictionary,
+    objects: &'b BTreeMap<lopdf::ObjectId, lopdf::Object>,
+    indirects: &mut Vec<(Ref, &'b lopdf::Object)>,
+) {
+    for (key, val) in from.into_iter() {
+        convert_obj(ref_counter, into.insert(Name(key)), val, objects, indirects)
+    }
 }
 
 /// Convert from one library to another
-fn convert_obj<'a>(from: &lopdf::Object, into: Obj<'a>) {
+fn convert_obj<'a, 'b: 'a>(
+    ref_counter: &mut Ref,
+    into: Obj<'a>,
+    from: &'b lopdf::Object,
+    objects: &'b BTreeMap<lopdf::ObjectId, lopdf::Object>,
+    indirects: &mut Vec<(Ref, &'b lopdf::Object)>,
+) {
     use lopdf::Object;
     match from {
         Object::Null => {}
@@ -133,16 +196,20 @@ fn convert_obj<'a>(from: &lopdf::Object, into: Obj<'a>) {
         Object::Array(v) => {
             let mut arr = into.array();
             for elem in v {
-                convert_obj(elem, arr.push());
+                convert_obj(ref_counter, arr.push(), elem, objects, indirects);
             }
         }
-        Object::Dictionary(v) => {
-            let mut dict = into.dict();
-            for (key, val) in v.into_iter() {
-                convert_obj(val, dict.insert(Name(key)))
-            }
+        Object::Dictionary(v) => convert_dict(ref_counter, into.dict(), v, objects, indirects),
+        Object::Stream(_v) => {
+            let id = ref_counter.bump();
+            indirects.push((id, from));
+            into.primitive(id);
         }
-        Object::Stream(_v) => todo!("streams are not yet implemented"),
-        Object::Reference(v) => into.primitive(Ref::new(v.0 as _)),
+        Object::Reference(v) => {
+            let obj = objects.get(v).unwrap();
+            let id = ref_counter.bump();
+            indirects.push((id, obj));
+            into.primitive(id)
+        }
     }
 }
